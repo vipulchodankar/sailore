97982f8ef4541c57868528a3a2575b9b9eda0437
Vipul Chodankar <vipul72c@gmail.com>
2020-10-09 10:46:54 +0530

    Home page and navbar

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,22 +1,28 @@
-import React, { FC, lazy } from "react";
+import React, { FC, lazy, Fragment } from "react";
 import { Switch, Route } from "react-router-dom";
+
+// Custom Components
+import NavBar from "./NavBar";
 
 import routes from "../config/routes";
 
 const Layout: FC = () => {
   return (
-    <Switch>
-      {routes.map((route) => (
-        <Route
-          exact
-          path={route.path}
-          component={route.component}
-          key={route.path}
-        />
-      ))}
+    <Fragment>
+      <NavBar />
+      <Switch>
+        {routes.map((route) => (
+          <Route
+            exact
+            path={route.path}
+            component={route.component}
+            key={route.path}
+          />
+        ))}
 
-      <Route component={lazy(() => import("../pages/404"))} />
-    </Switch>
+        <Route component={lazy(() => import("../pages/404"))} />
+      </Switch>
+    </Fragment>
   );
 };
 
--- frontend/src/pages/home/index.tsx
+++ frontend/src/pages/home/index.tsx
@@ -1,19 +1,48 @@
-import React, { FC } from "react";
+import React, { FC, Fragment } from "react";
+import { useHistory } from "react-router-dom";
 
 // Mui
+import { makeStyles } from "@material-ui/core";
+import Box from "@material-ui/core/Box";
 import Button from "@material-ui/core/Button";
 
+const useStyles = makeStyles((theme) => ({
+  landing: {
+    background:
+      "linear-gradient(rgba(0, 0, 0, 0.5),rgba(0, 0, 0, 0.5)), url('https://source.unsplash.com/1600x900/?sailor')",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+  },
+}));
+
 const HomePage: FC = () => {
+  const classes = useStyles();
+  const history = useHistory();
+
+  const handleClick = () => {
+    history.push("/sailor");
+  };
+
   return (
-    <div>
-      Home Page{" "}
-      <Button variant="contained" color="primary">
-        Button
-      </Button>
-      <Button variant="contained" color="secondary">
-        Button
-      </Button>
-    </div>
+    <Fragment>
+      <Box
+        width="100%"
+        height="100vh"
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        className={classes.landing}
+      >
+        <Button
+          variant="contained"
+          color="primary"
+          size="large"
+          onClick={handleClick}
+        >
+          View all sailors
+        </Button>
+      </Box>
+    </Fragment>
   );
 };
 
