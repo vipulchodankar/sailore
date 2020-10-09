57e727777a34c9e565a4a1e365f49f0a27e2381d
Vipul Chodankar <vipul72c@gmail.com>
2020-10-09 11:08:33 +0530

    Added cors to backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,21 @@
-import express, { Application } from "express";
+import express, { Application, Request, Response } from "express";
 import bodyParser from "body-parser";
 import { PORT } from "./config/server";
 import { logger } from "./middleware";
+import cors from "cors";
 import apiRouter from "./routes";
 
 const app: Application = express();
 
 app.use(bodyParser.json());
+app.use(cors());
 app.use(logger);
 
 app.use("/api", apiRouter);
+
+app.get("*", (req: Request, res: Response) => {
+  res.send("");
+});
 
 app.listen(PORT, () => {
   console.info(`Server running at http://localhost:${PORT}/`);
