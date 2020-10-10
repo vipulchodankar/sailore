9fec8a572eb79cc4e69a43cb6e660997b9f7ed76
Vipul Chodankar <vipul72c@gmail.com>
2020-10-10 21:10:04 +0530

    Updated meta and registered PWA

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,6 @@ import { Router } from "react-router-dom";
 import history from "./utils/history";
 
 // Redux
-// Redux
 import { Provider } from "react-redux";
 import store, { persistor } from "./redux";
 import { PersistGate } from "redux-persist/integration/react";
