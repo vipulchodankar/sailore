c63fa5e1e06a891ffb3086eb4a22dfa01500d534
Vipul Chodankar <vipul72c@gmail.com>
2020-10-10 19:40:03 +0530

    Added fuzzy search with fuse

--- frontend/src/pages/sailor/index.tsx
+++ frontend/src/pages/sailor/index.tsx
@@ -1,4 +1,5 @@
-import React, { FC, useEffect } from "react";
+import React, { FC, useEffect, useState } from "react";
+import Fuse from "fuse.js";
 
 // Redux
 import { useDispatch, useSelector } from "react-redux";
@@ -19,7 +20,12 @@ import Box from "@material-ui/core/Box";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import Fab from "@material-ui/core/Fab";
+import TextField from "@material-ui/core/TextField";
 import AddIcon from "@material-ui/icons/Add";
+import IconButton from "@material-ui/core/IconButton";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import ClearIcon from "@material-ui/icons/Clear";
+import SearchIcon from "@material-ui/icons/Search";
 
 // Custom Components
 import Loader from "../../components/Loader";
@@ -45,6 +51,15 @@ const SailorPage: FC = () => {
   const sailors = useSelector(selectSailorsList);
   const loading = useSelector(selectSailorsIsLoading);
   const dispatch = useDispatch();
+  const [search, setSearch] = useState("");
+
+  const fuseOptions = {
+    includeScore: true,
+    keys: ["SID", "SNAME", "RATING", "AGE"],
+  };
+
+  const fuse = new Fuse(sailors, fuseOptions);
+  const matchedSailors = fuse.search(search);
 
   useEffect(
     function loadSailors() {
@@ -67,17 +82,64 @@ const SailorPage: FC = () => {
         <Box py={8}>
           <Typography align="center">No sailors found</Typography>
         </Box>
-      ) : null}
-
-      <Box py={4}>
-        <Grid container spacing={4}>
-          {sailors.map((sailor: Sailor) => (
-            <Grid item xs={12} sm={6} md={4} xl={3} key={sailor.SID}>
-              <SailorCard {...sailor} />
+      ) : (
+        <Box py={4}>
+          <Box pb={4}>
+            <Grid container spacing={4}>
+              <Grid item xs={12} sm={6} md={4} xl={3}>
+                <TextField
+                  value={search}
+                  onChange={(e) => setSearch(e.target.value)}
+                  label="Search"
+                  fullWidth
+                  placeholder="Juan Wick"
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position="start">
+                        <SearchIcon />
+                      </InputAdornment>
+                    ),
+                    endAdornment: (
+                      <InputAdornment position="start">
+                        {search.length > 0 ? (
+                          <IconButton>
+                            <ClearIcon
+                              fontSize="small"
+                              onClick={() => setSearch("")}
+                            />
+                          </IconButton>
+                        ) : null}
+                      </InputAdornment>
+                    ),
+                  }}
+                />
+              </Grid>
             </Grid>
-          ))}
-        </Grid>
-      </Box>
+          </Box>
+
+          <Grid container spacing={4}>
+            {matchedSailors.length === 0
+              ? sailors.map((sailor: Sailor) => (
+                  <Grid item xs={12} sm={6} md={4} xl={3} key={sailor.SID}>
+                    <SailorCard {...sailor} />
+                  </Grid>
+                ))
+              : matchedSailors.map((matchedSailor: any) => (
+                  <Grid
+                    item
+                    xs={12}
+                    sm={6}
+                    md={4}
+                    xl={3}
+                    key={matchedSailor.item.SID}
+                  >
+                    <SailorCard {...matchedSailor.item} />
+                  </Grid>
+                ))}
+          </Grid>
+        </Box>
+      )}
+
       <Fab
         variant="extended"
         color="primary"
