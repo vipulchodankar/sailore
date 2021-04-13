f867f994e50924cf180ecc46ac818a5635f4f8a6
D-coder22 <amitnmf@gmail.com>
2021-04-14 02:58:08 +0530

    Fixed errors

--- flask-backend/routes/__init__.py
+++ flask-backend/routes/__init__.py
@@ -1,9 +1,11 @@
 from flask import Blueprint, Response, request, jsonify
 import mariadb
 from ..models.sailor import sailor as sailorSchema
-from ..config.db import cur
+from ..config.db import conn
 
-router = Blueprint("router")
+cur = conn.cursor(named_tuple=True)
+
+router = Blueprint("router", __name__,  url_prefix="/api")
 
 
 @router.route('/createSailorTable', methods=['PATCH'])
@@ -20,8 +22,8 @@ def createSailorTable():
         return res, 409
 
 
-@router.route('/sailor', methods=['POST'])
-def postSailor():
+@router.route('/sailor', methods=['POST', 'GET'])
+def Sailor():
     if request.method == "POST":
         try:
             req_data = request.get_json()
@@ -30,33 +32,74 @@ def postSailor():
             sailor['RATING'] = req_data['RATING']
             sailor['AGE'] = req_data['AGE']
 
-            sailorSchema.validate(sailor)
+            sailor = sailorSchema.validate(sailor)
+            cur.execute("INSERT INTO SAILOR (SNAME, RATING, AGE) VALUES (?, ?, ?)",
+                        (sailor['SNAME'], sailor['RATING'], sailor['AGE']))
+            conn.commit()
 
-            insertId = cur.execute("INSERT INTO SAILOR(SNAME, RATING, AGE) values(?, ?, ?)",
-                                   [sailor["SNAME"], sailor["RATING"], sailor["AGE"]])
+            insertID = cur.lastrowid
+            cur.execute(
+                "SELECT * FROM SAILOR WHERE SID =?", (insertID,))
 
-            data: any = cur.execute(
-                "SELECT * FROM SAILOR WHERE SID = ?", [insertId])
+            row = cur.fetchone()
+            data = []
+            sailor = {}
+
+            sailor["SNAME"] = row[1]
+            sailor["RATING"] = row[2]
+            sailor["AGE"] = row[3]
+            data.append(sailor)
 
             res = {"data": data,
-                   "message": "Sailor Successfully created"}
+                   "message": "Sailor Successfully Added"}
             return res, 200
         except Exception as e:
             print(Exception)
             return jsonify(e), 500
+
+    if request.method == "GET":
+        try:
+            cur.execute("SELECT * FROM SAILOR")
+            data = []
+            sailor = {}
+            rows = cur.fetchall()
+
+            for row in rows:
+                sailor = {}
+                sailor["SID"] = row.SID
+                sailor["SNAME"] = row.SNAME
+                sailor["RATING"] = row.RATING
+                sailor["AGE"] = row.AGE
+                data.append(sailor)
+
+            res = {"data": data,
+                   "message": "Sailors Successfully fetched"}
+            return res, 200
+        except Exception as e:
+            print(e)
+            res = jsonify(e)
+            return e, 500
 
 
 @router.route('/sailor/<id>', methods=['GET', 'PUT', 'DELETE'])
 def getSailor(id):
     if request.method == "GET":
         try:
-            data: any = cur.execute("SELECT * FROM SAILOR WHERE SID = ?", [id])
+            cur.execute("SELECT * FROM SAILOR WHERE SID = ?", [id])
 
-            if len(data) == 0:
+            row = cur.fetchone()
+
+            if not row:
                 res = {"message": "No Sailor Found"}
                 return res, 404
 
-            res = {"data": data[0], "message": "Sailor Successfully Fetched"}
+            data = {}
+            data["SID"] = row.SID
+            data["SNAME"] = row.SNAME
+            data["RATING"] = row.RATING
+            data["AGE"] = row.AGE
+
+            res = {"data": data, "message": "Sailor Successfully Fetched"}
 
             return res, 200
         except Exception as e:
@@ -74,11 +117,12 @@ def getSailor(id):
 
             sailorSchema.validate(sailor)
 
-            data: any = cur.execute("UPDATE SAILOR SET SNAME = ?, RATING = ?, AGE = ? WHERE SID = ?",
-                                    [sailor["SNAME"], sailor["RATING"], sailor["AGE"], id])
+            cur.execute("UPDATE SAILOR SET SNAME = ?, RATING = ?, AGE = ? WHERE SID = ?",
+                        (sailor["SNAME"], sailor["RATING"], sailor["AGE"], id))
 
-            res = {"data": data,
-                   "message": " Sailor Successfully Updated"}
+            conn.commit()
+
+            res = {"message": " Sailor Successfully Updated"}
 
             return res, 200
         except Exception as e:
@@ -88,10 +132,10 @@ def getSailor(id):
 
     if request.method == 'DELETE':
         try:
-            data: any = cur.execute("DELETE FROM SAILOR WHERE SID = ?", [id])
+            cur.execute("DELETE FROM SAILOR WHERE SID = ?", [id])
 
-            res = {"data": data,
-                   "message": "Sailor Successfully Deleted"}
+            conn.commit()
+            res = {"message": "Sailor Successfully Deleted"}
             return res, 200
         except Exception as e:
             print(e)
