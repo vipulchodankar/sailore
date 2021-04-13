b3930144389f68ea3e4098044df3201ae62c88ee
D-coder22 <amitnmf@gmail.com>
2021-04-14 02:58:33 +0530

    Entry point for flask added

---
+++ flask-backend/__init__.py
@@ -0,0 +1,13 @@
+from .routes import router
+from flask import Flask
+from flask_cors import CORS
+from .config.server import PORT
+
+app = Flask(__name__)
+CORS(app)
+
+
+app.register_blueprint(router)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=6000, debug=True)
